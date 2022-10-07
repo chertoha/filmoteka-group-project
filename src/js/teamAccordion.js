9a6e5d962b3ebdd2485c6e970f4f1fe525c51dbb
Vira Voronova <vera.voronova@hotmail.com>
2022-10-07 19:00:49 +0200

    teamModal

--- src/js/teamAccordion.js
+++ src/js/teamAccordion.js
@@ -3,6 +3,19 @@ const accordionRef = document.querySelector('#accordion');
 accordionRef.addEventListener('click', onBtnClick);
 
 function onBtnClick(event) {
+  // close previous item automatically
+  // перевірити роботу відкриття-закриття
+  // коли повторно нажимати на ту ж кнопку - не закривається
+
+  // Array.from(accordionRef.children).forEach(item => {
+  //   // if (item.classList.contains('is-shown')) {
+  //   //   item.classList.remove('is-shown');
+  //   //   item.childNodes[1].setAttribute('aria-expanded', 'false');
+  //   // }
+  //   item.classList.remove('is-shown');
+  //   item.childNodes[1].setAttribute('aria-expanded', 'false');
+  // });
+
   // open current item
   if (
     event.target.nodeName === 'BUTTON' ||
