d2740b9964c83c9d06f0183e6e1e8c4d1c3cc85a
Vira Voronova <vera.voronova@hotmail.com>
2022-10-07 20:21:17 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // do not write code here
 // only import can be inserted
-
 //Anton's temporary test code----------------------------
 // import './js/temp/antonTempTest';
 //Anton's temporary test code----------------------------
