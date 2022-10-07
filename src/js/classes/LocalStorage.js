bc8b68339a06a03702e93bd713a25af9a3f68dfa
Vira Voronova <vera.voronova@hotmail.com>
2022-10-07 19:26:11 +0200

    classes sample

---
+++ src/js/classes/LocalStorage.js
@@ -0,0 +1,11 @@
+class LocalStorage {
+  constructor() {
+    this.watchedItems = [];
+    this.queueItems = [];
+    this.itemsOnCurrentPage = [];
+  }
+
+  addItemToKeyStorage() {}
+  getItemFromKeyStorage() {}
+  removeItemFromKeyStorage() {}
+}
---
+++ src/js/classes/Modal.js
@@ -0,0 +1,14 @@
+class Modal {
+  constructor(selectors) {
+    // get selectors: Modal, btn open, btn close
+  }
+
+  addHandler() {
+    // add eventlistener
+  }
+
+  open() {}
+  close() {}
+  onEscapeKeyDown() {}
+  onBackdropClick() {}
+}
