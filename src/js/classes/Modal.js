8120610e99348e9dbfd977e7ce82706e1c8a453d
Vira Voronova <vera.voronova@hotmail.com>
2022-10-07 20:18:39 +0200

    fixed

--- src/js/classes/Modal.js
+++ src/js/classes/Modal.js
@@ -1,12 +1,12 @@
 class Modal {
-  // constructor(selectors) {
-  //   // get selectors: Modal, btn open, btn close
-  // }
-  // addHandler() {
-  //   // add eventlistener
-  // }
-  // open() {}
-  // close() {}
-  // onEscapeKeyDown() {}
-  // onBackdropClick() {}
+  constructor(selectors) {
+    // get selectors: Modal, btn open, btn close
+  }
+  addHandler() {
+    // add eventlistener
+  }
+  open() {}
+  close() {}
+  onEscapeKeyDown() {}
+  onBackdropClick() {}
 }
