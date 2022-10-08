48bfa593816c32c645d1567a6c5f1a87c5d7eea5
Vira Voronova <vera.voronova@hotmail.com>
2022-10-08 16:13:59 +0200

    test render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // do not write code here
 // only import can be inserted
 //Anton's temporary test code----------------------------
-// import './js/temp/antonTempTest';
+import './js/temp/antonTempTest';
 //Anton's temporary test code----------------------------
 
 import './js/API';
--- src/js/temp/antonTempTest.js
+++ src/js/temp/antonTempTest.js
@@ -1,91 +1,91 @@
-// //Anton's temporary code
-// const axios = require('axios').default;
-// import '../utils/handlebars-helpers';
-// import movieCardTemplate from '../../templates/movieCard.hbs';
+//Anton's temporary code
+const axios = require('axios').default;
+import '../utils/handlebars-helpers';
+import movieCardTemplate from '../../templates/movieCard.hbs';
 
-// //Pagination init---------------------------------------------
-// import Pagination from '../classes/pagination';
-// const container = document.querySelector('.pag');
-// const pagination = new Pagination(container);
-// pagination.on('aftermove', event => {
-//   console.log('page after move:', event.page);
-//   //here could be your code - api fetch movie with chosern page
-//   testMovieCardsTemplate(event.page);
-// });
-// pagination.on('beforemove', event => {
-//   console.log('page before move:', event.page);
-// });
-// //Pagination init---------------------------------------------
+//Pagination init---------------------------------------------
+import Pagination from '../classes/pagination';
+const container = document.querySelector('.pag');
+const pagination = new Pagination(container);
+pagination.on('aftermove', event => {
+  console.log('page after move:', event.page);
+  //here could be your code - api fetch movie with chosern page
+  testMovieCardsTemplate(event.page);
+});
+pagination.on('beforemove', event => {
+  console.log('page before move:', event.page);
+});
+//Pagination init---------------------------------------------
 
-// //--Variables-------------------------------------------------------
-// const API_KEY_TEST = '1936ce94882661ecfd75d2c22e8905aa';
-// const url_trendings = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY_TEST}`;
-// const url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY_TEST}`;
-// //--Variables-------------------------------------------------------
+//--Variables-------------------------------------------------------
+const API_KEY_TEST = '1936ce94882661ecfd75d2c22e8905aa';
+const url_trendings = `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY_TEST}`;
+const url_genres = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY_TEST}`;
+//--Variables-------------------------------------------------------
 
-// testMovieCardsTemplate();
+testMovieCardsTemplate();
 
-// //Test API----------------------------------------------------------
-// async function testMovieCardsTemplate(page = 1) {
-//   try {
-//     //Get genres
-//     const response = await axios.get(url_genres);
-//     const genres = response.data.genres;
-//     // console.log(genres);
+//Test API----------------------------------------------------------
+async function testMovieCardsTemplate(page = 1) {
+  try {
+    //Get genres
+    const response = await axios.get(url_genres);
+    const genres = response.data.genres;
+    // console.log(genres);
 
-//     //Get movies list
-//     const trendings = await axios.get(url_trendings + '&page=' + page);
-//     const movies = trendings.data.results;
-//     const totalMovies = trendings.data.total_results;
-//     console.log(trendings.data);
+    //Get movies list
+    const trendings = await axios.get(url_trendings + '&page=' + page);
+    const movies = trendings.data.results;
+    const totalMovies = trendings.data.total_results;
+    console.log(trendings.data);
 
-//     //Update movies genres_id with genres_name
-//     addGenreNamesToMovies(genres, movies);
+    //Update movies genres_id with genres_name
+    addGenreNamesToMovies(genres, movies);
 
-//     //Render template
-//     tempRenderCards(movies);
+    //Render template
+    tempRenderCards(movies);
 
-//     //Render Pagination
-//     pagination.updateTotalItems(totalMovies);
-//     pagination.render();
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-// //Test API----------------------------------------------------------
+    //Render Pagination
+    pagination.updateTotalItems(totalMovies);
+    pagination.render();
+  } catch (error) {
+    console.error(error);
+  }
+}
+//Test API----------------------------------------------------------
 
-// ////////////////=============IMPORTANT CODE PART================/////////////////////////////
+////////////////=============IMPORTANT CODE PART================/////////////////////////////
 
-// function tempRenderCards(movies) {
-//   const container = document.querySelector('.gallery__list');
+function tempRenderCards(movies) {
+  const container = document.querySelector('.gallery__list');
 
-//   //library = false if render gallery
-//   //library = true if render library
-//   container.innerHTML = movieCardTemplate({ movies, library: true });
-// }
+  //library = false if render gallery
+  //library = true if render library
+  container.innerHTML = movieCardTemplate({ movies, library: true });
+}
 
-// function addGenreNamesToMovies(genres, movies) {
-//   movies.forEach(movie => {
-//     console.log(movie.release_date);
+function addGenreNamesToMovies(genres, movies) {
+  movies.forEach(movie => {
+    console.log(movie.release_date);
 
-//     //Movies genres check
-//     movie.genres = [];
-//     for (let i = 0; i < movie.genre_ids.length; i++) {
-//       if (i === 3) {
-//         movie.genres[i - 1] = 'Others';
-//         break;
-//       }
-//       movie.genres.push(getGenreNameById(genres, movie.genre_ids[i]));
-//     }
+    //Movies genres check
+    movie.genres = [];
+    for (let i = 0; i < movie.genre_ids.length; i++) {
+      if (i === 3) {
+        movie.genres[i - 1] = 'Others';
+        break;
+      }
+      movie.genres.push(getGenreNameById(genres, movie.genre_ids[i]));
+    }
 
-//     //Year check
-//     if (movie.release_date) {
-//       movie.date = movie.release_date;
-//     }
-//   });
-// }
+    //Year check
+    if (movie.release_date) {
+      movie.date = movie.release_date;
+    }
+  });
+}
 
-// function getGenreNameById(genres, genreId) {
-//   return genres.find(genre => genre.id === genreId).name;
-// }
-// ////////////////////=============IMPORTANT CODE================/////////////////////////////
+function getGenreNameById(genres, genreId) {
+  return genres.find(genre => genre.id === genreId).name;
+}
+////////////////////=============IMPORTANT CODE================/////////////////////////////
