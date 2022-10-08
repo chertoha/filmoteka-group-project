6b907c632ed58f7a01e10505b32caa666ad06798
Denys Ostrianskyi <103590306+Denix200010@users.noreply.github.com>
2022-10-08 17:43:47 +0300

    Update ModalBtn.js

--- src/js/classes/ModalBtn.js
+++ src/js/classes/ModalBtn.js
@@ -1,5 +1,32 @@
-class ModalBtn {
-  toggleBtn() {
-    // change from "add to watch" to "remove from watch"
-  }
+// class ModalBtn {
+
+// }
+const  btnQueue = document.querySelector('.modal-btn__queue');
+const btnWatch = document.querySelector('.modal-btn__watch');
+
+
+btnQueue.addEventListener('click', onBtnQueueClick);
+btnWatch.addEventListener('click', onBtnWatchClick);
+
+function onBtnWatchClick(e) {
+  const dataWatch = [
+    // добавляем объекты с выбранными фильмами
+  ];
+  const currentFilms = {};
+  // формируем свойства объекта
+  //сделать невозможным повторное нажатие кнопки
+  dataWatch.push(currentFilms); 
+
+  const dataWatchSet = localStorage.setItem('watch', JSON.stringify(dataWatch));
+}
+
+function onBtnQueueClick(e) {
+  const dataQueue = [
+        // добавляем объекты с выбранными фильмами
+  ];
+  const currentFilms = {};
+  // формируем свойства объекта
+  //сделать невозможным повторное нажатие кнопки
+  dataWatch.push(currentFilms);
+  const dataQueueSet = localStorage.setItem('queue', JSON.stringify(dataQueue));
 }
