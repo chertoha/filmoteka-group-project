756a1bc53e2c98baf3b7d0cb4920ff0e81873ef5
Vira Voronova <vera.voronova@hotmail.com>
2022-10-10 15:16:46 +0200

    teamModal done

---
+++ src/js/classes/TeamList.js
@@ -0,0 +1,25 @@
+export default class TeamList {
+  #accordionRef = document.querySelector('#accordion');
+
+  addHandler() {
+    this.#accordionRef.addEventListener('click', this.onBtnClick);
+  }
+
+  onBtnClick(event) {
+    // open current item
+    if (
+      event.target.nodeName === 'BUTTON' ||
+      event.target.closest('[data-accordion-btn]')
+    ) {
+      const toggledBtnRef = event.target.closest('[data-accordion-btn]');
+
+      const attributeToSet =
+        toggledBtnRef.getAttribute('aria-expanded') === 'false'
+          ? 'true'
+          : 'false';
+
+      toggledBtnRef.setAttribute('aria-expanded', `${attributeToSet}`);
+      event.target.closest('li').classList.toggle('is-shown');
+    }
+  }
+}
--- src/js/teamAccordion.js
+++ src/js/teamAccordion.js
@@ -1,21 +1,4 @@
-const accordionRef = document.querySelector('#accordion');
+import TeamList from './classes/TeamList';
 
-accordionRef.addEventListener('click', onBtnClick);
-
-function onBtnClick(event) {
-  // open current item
-  if (
-    event.target.nodeName === 'BUTTON' ||
-    event.target.closest('[data-accordion-btn]')
-  ) {
-    const toggledBtnRef = event.target.closest('[data-accordion-btn]');
-
-    const attributeToSet =
-      toggledBtnRef.getAttribute('aria-expanded') === 'false'
-        ? 'true'
-        : 'false';
-
-    toggledBtnRef.setAttribute('aria-expanded', `${attributeToSet}`);
-    event.target.closest('li').classList.toggle('is-shown');
-  }
-}
+const teamAccordion = new TeamList();
+teamAccordion.addHandler();
