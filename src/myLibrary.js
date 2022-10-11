2d4332441858203a9579457280d9884c41b61c0c
AlexLunarr <102823767+AlexLunarr@users.noreply.github.com>
2022-10-12 00:04:24 +0300

    пагинация

--- src/myLibrary.js
+++ src/myLibrary.js
@@ -1,119 +1,88 @@
-// do not write code here
-// only import can be inserted
 import './js/utils/handlebars-helpers';
-
-//Anton's temporary test code----------------------------
-// import './js/temp/antonTempTest';
-//Anton's temporary test code----------------------------
-
 import './js/classes/HeaderBtnHandler';
 import './js/modal';
 import './js/teamAccordion';
-
 import Pagination from './js/classes/Pagination';
 import GalleryHandler from './js/classes/GalleryHandler';
 import template from './templates/movieCard.hbs';
 import { localStorageFilms } from './js/classes/ModalBtn';
+import Gallery from './js/classes/Gallery';
+
+localStorageFilms.saveItemsForArrayAfterReload();
 
 const galleryHandler = new GalleryHandler();
 galleryHandler.addGalleryHandler();
+
+const containerGallery = document.querySelector('.gallery__list');
+const gallery = new Gallery(containerGallery, template);
 
 const containerPag = document.querySelector('.pag');
 const pagination = new Pagination(containerPag);
 
 let currentPage = 1;
-let currentMovies = [];
-
+let total_results = load('watch');
 
 pagination.on('aftermove', event => {
   console.log(event.page);
   currentPage = event.page;
-
+  pageValue(total_results, currentPage)
 });
 
+function tempRenderCards(movies) {
+  const container = document.querySelector('.gallery__list');
+  // console.log(movies);
+  container.innerHTML = template({ movies, library: true });
+  
+  if (total_results.length > 20) {
+     pagination.updateTotalItems(total_results.length);
+  pagination.render();
+ }
+};
 
-// console.log('this is inside Library');
-// console.log('pagination', pagination);
-// console.log('template', template);
-// console.log('galleryHandler', galleryHandler);
 
-localStorageFilms.saveItemsForArrayAfterReload();
-
-import Spinner from './js/classes/spinner';
-import Gallery from './js/classes/Gallery';
-
-
-const containerGallery = document.querySelector('.gallery__list');
-const gallery = new Gallery(containerGallery, template);
-
-const keyOne = load('watch');
-const keyTwo = load('queue');
-let pageKeyOne;
-let pageKeyTwo;
-console.log(keyOne);
-
-// const containerPag = document.querySelector('.pag');
-// const pagination = new Pagination(containerPag);
-const spinner = new Spinner('.js-spinner');
-const perPage = 20;
-// ф-ция взять из локал стор
-export default function load(key) {
+function load(key) {
   try {
     const serializedState = localStorage.getItem(key);
     return serializedState === null ? undefined : JSON.parse(serializedState);
-    // 1 * 20 = 20
-    // 2 * 20 = 40
-    // 7 * 20 = 140
-    // if (page === 1) {return serializedState === null ? undefined : JSON.parse(serializedState)};
-    // if (page === 2) {return serializedState === null ? undefined : JSON.parse(serializedState) };
-    // serializedState === null ? undefined : JSON.parse(serializedState);
-
   } catch (error) {
     console.error('Get state error: ', error.message);
   }
-}
-
-// localStorage.clear();
-
-
-
-// console.log(keyOne)
-
-if (keyOne !== undefined) {
-  if (keyOne.length > 0) {
-    tempRenderCards(keyOne);
-    pageKeyOne = keyOne.length;
-    if (pageKeyOne > 20) {
-      
-      pagination.containerGallery
-      //  console.log(pageKeyOne);
-  //     pagination.on('beforemove', event => {
-  // console.log(event);
-
-  // tempRenderCards(keyOne);
-  //     })
-        ;
-
-    }
-    // tempRenderCards(keyOne);
-  }
 };
-if (keyTwo !== undefined) {
-  if (keyTwo.length > 0) {
-    pageKeyTwo = keyTwo.length;
-  }
+
+
+function audit(total_results, currentPage) {
+  if (total_results.length <= 20) {
+  tempRenderCards(total_results);
 }
-// console.log(pageKeyOne)
-// console.log(pageKeyTwo)
+else if (total_results.length > 20) {
+  pageValue(total_results, currentPage)
+}}
+
+function pageValue(total_results, currentPage) {
+  // console.log(currentPage);
+      const lastMovie = currentPage * 20;
+   const firstMovie = lastMovie - 20;
+  const moviesPars = [];
+  for (let index = firstMovie; index < lastMovie; index++) {
+    if (total_results[index] !== undefined) {
+    moviesPars.push(total_results[index]);
+  }
+   
+}
+  tempRenderCards(moviesPars);
+}
+
+
+audit(total_results, currentPage);
+
+
+// КНОПКИ
 const btn = document.querySelector('.header');
 const btnWatch = btn.querySelectorAll('.button--dark-mode');
-console.log(btnWatch);
+// console.log(btnWatch);
 
-
-
-
+// КНОПКИ
 btn.addEventListener("click", selectBtn);
-
 
 function selectBtn(event) {
   if (event.target.nodeName !== 'BUTTON') {
@@ -125,54 +94,18 @@ function selectBtn(event) {
 
   if (event.target.textContent === 'watched') {
     // console.log("watched")
-    event.target.classList.add('button--active');
-
-    tempRenderCards(keyOne);
-  } else if (event.target.textContent === 'queue') {
+    event.target.classList.add('button--active')
+    total_results = load('watch');
+    audit(total_results, currentPage);
+// console.log(currentPage)
+  }
+  else if (event.target.textContent === 'queue') {
     // console.log("queue");
     event.target.classList.add('button--active');
-    tempRenderCards(keyTwo);
+    total_results = load('queue');
+    audit(total_results, currentPage);
+    // console.log(currentPage)
   }
 }
 
-function tempRenderCards(movies) {
-  const lastMovie = currentPage * 20;
-   const firstMovie = lastMovie - 20;
-  const container = document.querySelector('.gallery__list');
-  const moviesPars = [];
-for (let index = firstMovie; index < lastMovie; index++) {
-
-  moviesPars.push(movies[index]);
-  
-}
-  console.log(moviesPars);
-  container.innerHTML = template({ movies, library: true });
-
-  // проверка на 20 
-  pagination.updateTotalItems(movies.length);
-  pagination.render();
-}
-
-
-// pagination.on('aftermove', event => {
-//   console.log(event);
-
-//   // fetchMovies(event);
-// });
-
-
-// fetchMovies();
-// function fetchMovies(page = 1) {
-//   // spinner.show();
-  
-//     // const movies = gallery.tempRenderCards(page);
-//     // // spinner.hide();
-//     // console.log(movies);
-//     // gallery.renderCards(movies.results);
-
-//     // pagination.updateTotalItems(movies.total_results);
-//     // pagination.render();
-// }
-
-// export { apiService, gallery };
 
