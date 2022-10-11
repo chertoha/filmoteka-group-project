27169b1c09ae3ccbdba4d3559e11ff92659cedf8
Vira Voronova <vera.voronova@hotmail.com>
2022-10-11 13:31:53 +0200

    gallery render made

--- src/js/classes/GalleryHandler.js
+++ src/js/classes/GalleryHandler.js
@@ -32,15 +32,11 @@ export default class GalleryHandler {
     const itemToFind = allCurrentItemsOnPage.find(item => item.id === +id);
     localStorageFilms.currentFilm = itemToFind;
 
-    console.log('itemToFind', itemToFind.genres);
     return itemToFind;
   }
 
   renderMovieCard(movie) {
-    // checkMovies.update(movie, this.genres);
     checkMovies.checkMoviePoster(movie);
-    console.log('movie.genres', movie.genres);
-
     this.#modalContent.innerHTML = modalMovieDetailsTemplate(movie);
   }
 }
