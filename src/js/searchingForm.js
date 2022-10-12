d3c58af43e45c4c953aed75ead55f3523067b003
Vira Voronova <vera.voronova@hotmail.com>
2022-10-12 10:37:40 +0200

    sarching form changed

--- src/js/searchingForm.js
+++ src/js/searchingForm.js
@@ -1,29 +1,29 @@
-import SearchingForm from './classes/SearchingForm';
+// import SearchingForm from './classes/SearchingForm';
 
-const searchingFormRef = document.querySelector('.search-form');
-const inputRef = document.querySelector('.search-input');
+// const searchingFormRef = document.querySelector('.search-form');
+// const inputRef = document.querySelector('.search-input');
 
-searchingFormRef.addEventListener('submit', onSearchSubmit);
+// searchingFormRef.addEventListener('submit', onSearchSubmit);
 
-const formSearch = new SearchingForm();
+// const formSearch = new SearchingForm();
 
-function onSearchSubmit(event) {
-  event.preventDefault();
+// function onSearchSubmit(event) {
+//   event.preventDefault();
 
-  let searchValue = inputRef.value;
-  searchValue.trim();
+//   let searchValue = inputRef.value;
+//   searchValue.trim();
 
-  formSearch.searchMovies('Killer');
-  if (searchValue == '') {
-    searchingFormRef.dataset.message =
-      'Search attempt not successful. Please, enter movie name.';
-    //
+//   formSearch.searchMovies('Killer');
+//   if (searchValue == '') {
+//     searchingFormRef.dataset.message =
+//       'Search attempt not successful. Please, enter movie name.';
+//     //
 
-    setTimeout(() => {
-      searchingFormRef.dataset.message = '';
-    }, 5500);
-    return;
-  }
+//     setTimeout(() => {
+//       searchingFormRef.dataset.message = '';
+//     }, 5500);
+//     return;
+//   }
 
-  formSearch.getMovies(searchValue, 1);
-}
+//   formSearch.getMovies(searchValue, 1);
+// }
