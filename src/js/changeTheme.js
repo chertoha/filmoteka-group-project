af31905a4e0f0c17741da57b09ae0fe20cccda91
YuriiSenenko <yurkosenenko@gmail.com>
2022-10-12 19:51:27 +0300

    new-update

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -1,14 +1,12 @@
 const refs = {
-  //   gallery: document.querySelector('.gallery__item'),
-  //   body: document.querySelector('body'),
   button: document.querySelector('.theme-btn'),
   header: document.querySelector('header'),
 };
 
-console.log(header);
 refs.button.addEventListener('click', changeTheme);
 
 function changeTheme(event) {
   document.body.classList.toggle('dark-theme');
   refs.header.classList.toggle('dark-image');
+  //   refs.header.classList.toggle('dark-image-lib');
 }
