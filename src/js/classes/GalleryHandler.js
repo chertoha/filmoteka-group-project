3667b25e998a1dd14d2d6e4906209c4756685a83
Vira Voronova <vera.voronova@hotmail.com>
2022-10-13 16:14:38 +0200

    flip card

--- src/js/classes/GalleryHandler.js
+++ src/js/classes/GalleryHandler.js
@@ -1,16 +1,19 @@
 import { localStorageFilms } from './ModalBtn';
 import { movieCardModal } from '../modal';
 import modalMovieDetailsTemplate from '../../templates/modalMovieCard.hbs';
+import { apiService } from '../gallery';
 
 export default class GalleryHandler {
   #galleryRef = document.querySelector('#gallery');
   #modalContent = document.querySelector('.js-movie-card');
 
   addGalleryHandler() {
-    this.#galleryRef.addEventListener('click', this.onMovieCardClick);
+    this.#galleryRef.addEventListener('click', event =>
+      this.onMovieCardClick(event)
+    );
   }
 
-  onMovieCardClick = event => {
+  async onMovieCardClick(event) {
     event.preventDefault();
 
     if (
@@ -19,16 +22,18 @@ export default class GalleryHandler {
     ) {
       const itemIdToFind = event.target.closest('a').dataset.movieId;
       this.renderMovieCard(this.findClickedItem(itemIdToFind));
-
-      localStorageFilms.onModalQueueBtnChange(
-        this.#modalContent.querySelector('.queue-js')
-      );
-      localStorageFilms.onModalWatchedBtnChange(
-        this.#modalContent.querySelector('.watched-js')
-      );
+      this.getModalsButtons();
+      this.FlipImgHandler();
       movieCardModal.openModal();
+
+      try {
+        const movie = await apiService.fetchMoviesByID(itemIdToFind);
+        console.log('data', movie.title);
+      } catch (error) {
+        console.log(error);
+      }
     }
-  };
+  }
 
   findClickedItem(id) {
     const allCurrentItemsOnPage = localStorageFilms.getItemFromKeyStorage(
@@ -36,12 +41,29 @@ export default class GalleryHandler {
     );
     const itemToFind = allCurrentItemsOnPage.find(item => item.id === +id);
     localStorageFilms.currentFilm = itemToFind;
-
     return itemToFind;
   }
 
   renderMovieCard(movie) {
     this.#modalContent.innerHTML = modalMovieDetailsTemplate(movie);
   }
-  1;
+
+  getModalsButtons() {
+    localStorageFilms.onModalQueueBtnChange(
+      this.#modalContent.querySelector('.queue-js')
+    );
+    localStorageFilms.onModalWatchedBtnChange(
+      this.#modalContent.querySelector('.watched-js')
+    );
+  }
+
+  FlipImgHandler() {
+    const flipCard = document.querySelector('#flip-wrapper');
+    flipCard.addEventListener('click', this.onImageClick);
+  }
+
+  onImageClick(evn) {
+    console.log(evn.currentTarget.classList);
+    evn.currentTarget.classList.toggle('is-flipped');
+  }
 }
